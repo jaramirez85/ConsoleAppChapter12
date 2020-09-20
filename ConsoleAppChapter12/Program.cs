c60bf5d6d693162fc766f9d5161a139a2161b613
jaramirez85 <jaramirez85@gmail.com>
2020-09-20 15:41:19 -0500

    A SPECIAL IDENTIFIER: _ DISCARDS

--- ConsoleAppChapter12/Program.cs
+++ ConsoleAppChapter12/Program.cs
@@ -8,16 +8,12 @@ namespace ConsoleAppChapter12
         static void Main(string[] args)
         {
 
-            (int a, int b, string name) = MethodReturningTuple();
-            //(int a, int b, var name) = MethodReturningTuple();   --implicit, explicit
-            //(long a, int b, XNamespace name) = MethodReturningTuple(); --with convertions
-            //var (a, b, name) = MethodReturningTuple();  --shorthand c#7
-
-            Console.WriteLine($"a: {a}; b: {b}; name: {name}");
+            //var _ = "Xavi";
+            var tuple = (1, 2, 3, 4);
+            var (x, y, _, _) = tuple;
+            Console.WriteLine(_);
 
             Console.ReadLine();
         }
-
-        static (int x, int y, string text) MethodReturningTuple() => (1, 2, "t");
     }
 }

82d68d501729713c3689375adabbaaebc909b710
jaramirez85 <jaramirez85@gmail.com>
2020-09-20 13:10:38 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleAppChapter12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppChapter12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = (10, "text");
+            var (a, b) = tuple;
+            (int c, string d) = tuple;
+
+            int e;
+            string f;
+            (e, f) = tuple;
+
+            Console.WriteLine($"a: {a}; b: {b}");
+            Console.WriteLine($"c: {c}; d: {d}");
+            Console.WriteLine($"e: {e}; f: {f}");
+            Console.ReadLine();
+        }
+    }
+}

770f4eeeaf5633969429192d7ef473ae8ff677d0
jaramirez85 <jaramirez85@gmail.com>
2020-09-20 21:10:59 -0500

    Listing 12.5 Deconstruction in which evaluation order matters

--- ConsoleAppChapter12/Program.cs
+++ ConsoleAppChapter12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Xml.Linq;
 
 namespace ConsoleAppChapter12
@@ -8,18 +9,20 @@ namespace ConsoleAppChapter12
         static void Main(string[] args)
         {
 
-            var point = new Point(2, 3);
-            Console.WriteLine($"X: {point.X}");
-            Console.WriteLine($"Y: {point.Y}");
-            
+            StringBuilder builder = new StringBuilder("12345");
+            StringBuilder original = builder;
+            (builder, builder.Length) = (new StringBuilder("67890"), 3);
+            Console.WriteLine(original);
+            Console.WriteLine(builder);
+
+            //not all tuple literals have a type. For example, the tuple
+            //literal(null, x => x * 2) doesn’t have a type because neither of its element expressions
+            //has a type.
+            (string text, Func<int, int> func) = (null, x => x * 2);
+            (text, func) = ("text", x => x * 3);
+
             Console.ReadLine();
         }
 
-        public sealed class Point
-        {
-            public double X { get; }
-            public double Y { get; }
-            public Point(double x, double y) => (X, Y) = (x, y);
-        }
     }
 }

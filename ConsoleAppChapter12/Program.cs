6ad100eb5034e1c30c1263dc609142933e78a52d
jaramirez85 <jaramirez85@gmail.com>
2020-09-21 20:36:02 -0500

    Using an extension method to deconstruct DateTime and overloading

--- ConsoleAppChapter12/Program.cs
+++ ConsoleAppChapter12/Program.cs
@@ -3,26 +3,29 @@ using System.Xml.Linq;
 
 namespace ConsoleAppChapter12
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            var point = new Point(2, 3);
-            var (x, y) = point;
-            Console.WriteLine($"x = {x}");
-            Console.WriteLine($"y = {y}");
+            DateTime now = DateTime.UtcNow;
+            var (year, month, day) = now;
+
+            //Listing 12.9 Using Deconstruct overloads
+
+            //DateTime birthday = new DateTime(1976, 6, 19);
+            //DateTime now = DateTime.UtcNow;
+            //var (year, month, day, hour, minute, second) = now; --Uses the six-value deconstructor
+            //(year, month, day) = birthday;  --Uses the three-value deconstructor
+
+            Console.WriteLine($"{year:0000}-{month:00}-{day:00}");
 
             Console.ReadLine();
         }
 
-        public sealed class Point
-        {
-            public double X { get; }
-            public double Y { get; }
-            public Point(double x, double y) => (X, Y) = (x, y);
+        static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day)
+            => (year, month, day) = (dateTime.Year, dateTime.Month, dateTime.Day);
 
-            //The simplicity of this is beautiful
-            public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
-        }
+        static void Deconstruct(this DateTime dateTime, out int year, out int month, out int day, out int hour, out int minute, out int second) =>
+            (year, month, day, hour, minute, second) = (dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
     }
 }

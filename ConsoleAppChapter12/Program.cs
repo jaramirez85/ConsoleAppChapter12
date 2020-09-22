ffa24f19508d3b53460657c63a2bb8c2d3e132ad
jaramirez85 <jaramirez85@gmail.com>
2020-09-21 20:11:53 -0500

    Listing 12.4. Simple constructor assignments using deconstruction and a tuple literal

--- ConsoleAppChapter12/Program.cs
+++ ConsoleAppChapter12/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Xml.Linq;
 
 namespace ConsoleAppChapter12
@@ -8,21 +7,22 @@ namespace ConsoleAppChapter12
     {
         static void Main(string[] args)
         {
-
-            StringBuilder builder = new StringBuilder("12345");
-            StringBuilder original = builder;
-            (builder, builder.Length) = (new StringBuilder("67890"), 3);
-            Console.WriteLine(original);
-            Console.WriteLine(builder);
-
-            //not all tuple literals have a type. For example, the tuple
-            //literal(null, x => x * 2) doesn’t have a type because neither of its element expressions
-            //has a type.
-            (string text, Func<int, int> func) = (null, x => x * 2);
-            (text, func) = ("text", x => x * 3);
+            var point = new Point(2, 3);
+            var (x, y) = point;
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
 
             Console.ReadLine();
         }
 
+        public sealed class Point
+        {
+            public double X { get; }
+            public double Y { get; }
+            public Point(double x, double y) => (X, Y) = (x, y);
+
+            //The simplicity of this is beautiful
+            public void Deconstruct(out double x, out double y) => (x, y) = (X, Y);
+        }
     }
 }
